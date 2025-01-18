df024fc41d5833f0c5b6971fb6d9e353d771c77c
utkualavanda@gmail.com <utkualavanda@gmail.com>
2025-01-18 09:39:49 +0300

    feat: implement proper error handling

---
+++ src/features/Error/pages/SomethingWentWrong/SomethingWentWrongPage.tsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material';
+
+export const SomethingWentWrongPage = () => {
+  return (
+    <Typography>
+      Oops! Something Went Wrong.
+      <br />
+      Don’t worry! This might be a temporary issue. Here are a few things you
+      can try:
+      <br />
+      Refresh the page - Go back to the previous page - Contact us if the
+      problem persists.
+    </Typography>
+  );
+};
