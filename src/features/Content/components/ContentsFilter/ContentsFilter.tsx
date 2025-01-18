11cc4b3880404e241f9afa2cfd32785852310ad1
utkualavanda@gmail.com <utkualavanda@gmail.com>
2025-01-18 10:47:10 +0300

    fix: resolve useForm value initialization issue

--- src/features/Content/components/ContentsFilter/ContentsFilter.tsx
+++ src/features/Content/components/ContentsFilter/ContentsFilter.tsx
@@ -1,3 +1,5 @@
+import ClearIcon from '@mui/icons-material/Clear';
+import { IconButton } from '@mui/material';
 import Button from '@mui/material/Button';
 import FormControl from '@mui/material/FormControl';
 import Grid2 from '@mui/material/Grid2';
@@ -5,7 +7,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 
 import { useBreakpoints } from '../../../../hooks/utils/useBreakpoints';
 import {
@@ -20,13 +22,15 @@ export const ContentsFilter = ({
 }) => {
   const { isDesktop } = useBreakpoints();
 
-  const { register, handleSubmit } = useForm({
+  const { handleSubmit, control, watch, setValue } = useForm({
     defaultValues: {
       year: defaultFilterValue.year,
       search: defaultFilterValue.search,
       type: defaultFilterValue.type,
     },
   });
+
+  const typeWatchValue = watch('type');
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -37,32 +41,64 @@ export const ContentsFilter = ({
         alignItems="center"
       >
         <Grid2 size={isDesktop ? 2 : 12}>
-          <TextField fullWidth label="Search" {...register('search')} />
+          <Controller
+            name="search"
+            control={control}
+            render={({ field }) => (
+              <TextField {...field} fullWidth label="Search" />
+            )}
+          />
         </Grid2>
         <Grid2 size={isDesktop ? 2 : 12}>
-          <TextField fullWidth label="Year" {...register('year')} />
+          <Controller
+            name="year"
+            control={control}
+            render={({ field }) => (
+              <TextField {...field} fullWidth label="Year" />
+            )}
+          />
         </Grid2>
         <Grid2 size={isDesktop ? 2 : 12}>
-          <FormControl fullWidth>
-            <InputLabel id="type-select">Type</InputLabel>
-            <Select labelId="type-select" label="Type" {...register('type')}>
-              <MenuItem value="movie">Movie</MenuItem>
-              <MenuItem value="series">Series</MenuItem>
-              <MenuItem value="episode">Episode</MenuItem>
-            </Select>
-          </FormControl>
+          <Controller
+            name="type"
+            control={control}
+            render={({ field }) => (
+              <FormControl fullWidth>
+                <InputLabel id="type-select">Type</InputLabel>
+                <Select
+                  {...field}
+                  labelId="type-select"
+                  label="Type"
+                  endAdornment={
+                    !!typeWatchValue && (
+                      <IconButton
+                        sx={{ padding: 0, marginRight: '12px' }}
+                        onClick={() =>
+                          setValue('type', defaultFilterValue.type)
+                        }
+                      >
+                        <ClearIcon />
+                      </IconButton>
+                    )
+                  }
+                >
+                  <MenuItem value="movie">Movie</MenuItem>
+                  <MenuItem value="series">Series</MenuItem>
+                  <MenuItem value="episode">Episode</MenuItem>
+                </Select>
+              </FormControl>
+            )}
+          />
         </Grid2>
-        <Grid2>
-          <Button
-            variant="contained"
-            size="large"
-            color="secondary"
-            type="submit"
-            fullWidth={!isDesktop}
-          >
-            Submit
-          </Button>
-        </Grid2>
+        <Button
+          variant="contained"
+          size="large"
+          color="secondary"
+          type="submit"
+          fullWidth={!isDesktop}
+        >
+          Submit
+        </Button>
       </Grid2>
     </form>
   );
